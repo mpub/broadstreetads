9afa2002b3f699e9e4034ed54ca5a681eacc2a25
Brian Sutherland <brian@vanguardistas.net>
2014-06-11 10:56:07 +0200

    support python 3.4

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(name="broadstreetads",
           "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
           ],
       test_suite='tests',
       )
